7d9eed6270e39a41446bfd2be8193746be0c8d47
Ender <torresender96@gmail.com>
2024-03-20 21:56:34 -0300

    vistas agregadas

--- Cliente/src/pages/indexPage/IndexPage.jsx
+++ Cliente/src/pages/indexPage/IndexPage.jsx
@@ -1,25 +1,13 @@
-import { useContext } from "react";
-import { ECommerceContext } from "../../context/ContextProyect";
-import Carrusel from "../../components/carrusel/Carrusel";
-let slides = [
-    "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
-    "https://wallpapercave.com/wp/wp3386769.jpg",
-    "https://wallpaperaccess.com/full/809523.jpg",
-    "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
-  ];
-
+import Body from "../../components/body/Body";
 const IndexPage = () => {
 
-	const {pruebaContext} =  useContext(ECommerceContext)
 
 	return (
 		<div>
-			<div className=" m-auto pt-11 w-[100%]">
-			<Carrusel slides={slides}/>
-			</div>
-			<h2>{pruebaContext}</h2>
+			<Body/>
+			
 		</div>
 	);
-};
+}; 
 
 export default IndexPage;
