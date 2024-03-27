d81c2ed4ef11bedb526568af3fafecf31c29dbf1
maximo cavero <cavero.maximo.l@gmail.com>
2024-03-26 22:46:53 -0300

    reparando lo del carro

--- Cliente/src/components/carro/Carro.jsx
+++ Cliente/src/components/carro/Carro.jsx
@@ -36,7 +36,7 @@ const Carro = () => {
                                 </td>
                                 <td class="py-4">$19.99</td>
                             </tr>
-                            <!-- More product rows -->
+                          
                         </tbody>
                     </table>
                 </div>
@@ -71,4 +71,4 @@ const Carro = () => {
   )
 }
 
-export default Carro
+export default Carro;
---
+++ Cliente/src/pages/carroPage/CarroPage.jsx
@@ -0,0 +1,11 @@
+import Carro  from '../../components/carro/Carro'
+
+const CarroPage = () => {
+  return (
+    <>
+    <Carro></Carro>
+    </>
+  )
+}
+
+export default CarroPage
